6408999ad7adebefea6d82137a8f0d00b03f6b91
SebastianCaballero <sebastian.caballero7@outlook.com>
2020-10-20 21:34:10 -0500

    Se establece la función no_literales en tableux.py

--- tableaux.py
+++ tableaux.py
@@ -91,7 +91,12 @@ def no_literales(l):
 	# solo literales
 	# Input: l, una lista de fórmulas como árboles
 	# Output: None/f, tal que f no es literal
-	return False
+	for formula in l:
+		if (es_literal(formula)):
+			continue
+		else:
+			return formula
+	return None
 
 def clasifica_y_extiende(f):
 	# clasifica una fórmula como alfa o beta y extiende listaHojas
